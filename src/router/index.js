4ef85342f31b184a858d2e8f924a204ce3901622
胡功宁 <hugongning@dongqiudi.com>
2017-08-21 15:02:21 +0800

    添加一个测试模块 同事 添加vue的eslin检查配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: () => {
+        import ('@/components/Hello')
+      }
+    },
+
+
+    {
+      path: '/Swiper',
+      name: 'Swiper',
+      component: () => {
+        import ('@/components/Swiper')
+      }
     }
   ]
 })
